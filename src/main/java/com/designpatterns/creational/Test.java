f5ed42224bf941e4984f0d14025dacbf2750a180
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2024-03-17 09:46:01 +0530

    initial

--- src/main/java/com/designpatterns/creational/Test.java
+++ src/main/java/com/designpatterns/creational/Test.java
@@ -3,5 +3,6 @@ package com.designpatterns.creational;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Design Patterns - Creational Patterns");
+        System.out.println("1. Singleton Pattern");
     }
 }
